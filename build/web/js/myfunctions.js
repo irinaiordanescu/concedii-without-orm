54ccffbd66e2a51e1a65b2dbdaf2b442b44a123a
Cristian <cle@novicell.dk>
2018-07-09 20:17:44 +0300

    Irina touch

--- build/web/js/myfunctions.js
+++ build/web/js/myfunctions.js
@@ -34,7 +34,7 @@ function doLogin() {
         ,
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             alert ("EROARE");
-            console.log(textStatus); 
+            console.log(textStatus);
         }
     });
 }
